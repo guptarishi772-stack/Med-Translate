4621e7f108d13a56b316482e773230108ba6f12c
Rishi Gupta <guptarishi772@gmail.com>
2026-03-31 11:47:42 +0530

    Added secrets vault and fixed arguments

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ left_col, right_col = st.columns([1, 2])
 
 with left_col:
 
-    api_key = st.text_input("Enter Gemini API key", type="password")
+    api_key = st.secrets["GEMINI_API_KEY"]
     uploaded_file = st.file_uploader("upload Medical Report PDF", type=['pdf'])
 
 with right_col:
 
-    if uploaded_file and api_key:
+    if uploaded_file:
         if st.button("Translate Report"):
             with open("temp.pdf", "wb") as f:
                 f.write(uploaded_file.getvalue())
 
     
-            result = translate_medical_report("temp.pdf", api_key) 
+            result = translate_medical_report(api_key, "temp.pdf") 
             st.write(result)   
